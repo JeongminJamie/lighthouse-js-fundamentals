a30b84260106ab5ef8ea405407a065116f6f319e
JeongminJamie <sksrhcalsu@gmail.com>
2022-04-15 15:05:57 -0400

    packing

--- packing-list.js
+++ packing-list.js
@@ -5,10 +5,3 @@ for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 packingList[3];
-
-console.log("Kitchen stuff to pack:");
-let i = 0;
-while (i < packingList.length) {
-  console.log(packingList[i]);
-  i++;
-}
